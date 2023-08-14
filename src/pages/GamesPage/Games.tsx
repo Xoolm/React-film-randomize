d531425fa4a402f4af091d1ca8e8e94cc0070a1e
Andrey Dunaev <andreydunaev5@gmail.com>
2023-08-14 17:08:37 +0300

    adapation reliase v0.0

--- src/pages/GamesPage/Games.tsx
+++ src/pages/GamesPage/Games.tsx
@@ -7,9 +7,11 @@ import { useContext, useMemo, useState } from "react";
 import MysteryCardWrapp from "../../components/Games/MysteryCardWrapp/MysteryCardWrapp";
 import { NumbersContext } from "../../context";
 import { IFilm } from "../../models/IFilm";
+import Select from "react-select";
+import { InputActionMeta } from "react-select/dist/declarations/src";
 
 const FilmsTemplate = () => {
-  const [game, setGame] = useState<number>(1);
+  const [game, setGame] = useState<any>(0);
   const { allFilms } = useContext(NumbersContext);
   const [filmPlate, setFilmPlate] = useState<IFilm[]>([]);
   useMemo(() => setFilmPlate(allFilms), [allFilms]);
@@ -20,6 +22,12 @@ const FilmsTemplate = () => {
         numbers.push(film.id);
       }
     });
+  const options = [
+    { value: 1, label: "Карточки на выбывание" },
+    { value: 2, label: "Колесо рандома" },
+    { value: 3, label: "Выбор карточек" },
+  ];
+  console.log(game);
 
   return (
     <AnimatedPage>
@@ -31,30 +39,52 @@ const FilmsTemplate = () => {
             alignItems: "center",
           }}
         >
-          <select
+          <Select
             className={Games.selectGame}
+            options={options}
             value={game}
-            onChange={(e) => setGame(Number(e.target.value))}
-          >
-            <option value={1}>Карточки на выбывание</option>
-            <option value={2}>Колесо рандома</option>
-            <option value={3}>Выбор карточек</option>
-          </select>
+            onChange={setGame}
+            placeholder="Выберите вид игры"
+            styles={{
+              control: (baseStyles, state) => ({
+                ...baseStyles,
+                borderRadius: "20px",
+                padding: "5px 30px",
+              }),
+            }}
+          />
+          {/* <div className={Games.selectWrapper}>
+            <select
+              className={Games.selectGame}
+              value={game}
+              onChange={(e) => setGame(Number(e.target.value))}
+            >
+              <option disabled value={0}>
+                Выберите игру
+              </option>
+              <option value={1}>Карточки на выбывание</option>
+              <option value={2}>Колесо рандома</option>
+              <option value={3}>Выбор карточек</option>
+            </select>
+          </div> */}
           <div className={Games.templateWrapp}>
-            {game === 1 ? (
+            {game.value === 1 ? (
               <RandomCardOutWrapp
                 filmPlate={filmPlate}
                 numbers={numbers}
                 setFilmPlate={setFilmPlate}
               />
             ) : null}
-            {game === 3 ? <MysteryCardWrapp /> : null}
-            {game === 2 ? (
+            {game.value === 3 ? <MysteryCardWrapp /> : null}
+            {game.value === 2 ? (
               <RandomWheelWrapp
                 filmPlate={filmPlate}
                 numbers={numbers}
                 setFilmPlate={setFilmPlate}
               />
+            ) : null}
+            {game.value === 0 ? (
+              <h3 style={{ color: "white" }}>Выберите игру</h3>
             ) : null}
           </div>
         </Container>
